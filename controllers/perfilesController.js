727ddbf3dfe485e7d6f0cbb4abf5408327f0fab8
Pablocb541 <chaconp560@gmail.com>
2024-03-18 22:24:48 -0600

    commit 12

--- controllers/perfilesController.js
+++ controllers/perfilesController.js
@@ -8,13 +8,22 @@ const UsuarioRestringido = require("../models/perfilesModel");
  */
 const usuarioRestringidoPost = async (req, res) => {
   const { nombreCompleto, pin, avatar, edad } = req.body;
+  const errorMessages = [];
 
   // Validación de campos requeridos
   if (!nombreCompleto || !pin || !avatar || !edad) {
-    return res.status(400).json({ error: "Todos los campos marcados con (*) son requeridos." });
+    errorMessages.push("Todos los campos marcados con (*) son requeridos.");
   }
 
-  // Otras validaciones pueden incluir la validación del formato del PIN, la edad, etc.
+  // Verificar si ya existe un usuario con el mismo nombre
+  const usuarioExistente = await UsuarioRestringido.findOne({ nombreCompleto });
+  if (usuarioExistente) {
+    errorMessages.push("Ya existe un usuario con el mismo nombre.");
+  }
+
+  if (errorMessages.length > 0) {
+    return res.status(400).json({ errors: errorMessages }); // Devolver mensajes de error
+  }
 
   try {
     // Crear un nuevo usuario restringido
@@ -92,9 +101,40 @@ const usuarioRestringidoDelete = async (req, res) => {
   }
 };
 
+/**
+ * Autentica un usuario restringido y envía la URL de redirección
+ *
+ * @param {*} req
+ * @param {*} res
+ */
+const loginPin = async (req, res) => {
+  const { pin } = req.body;
+  
+  try {
+    // Buscar el usuario restringido en la base de datos por su PIN
+    const usuario = await UsuarioRestringido.findOne({ pin });
+
+    // Verificar si el usuario existe
+    if (!usuario) {
+      return res.status(401).json({ error: "PIN inválido." });
+    }
+
+    // Si el PIN es válido, enviar la URL de redirección según el tipo de usuario
+    if (usuario.nombreCompleto === "administrador") {
+      res.status(200).json({ redirectURL: "./admin.html" }); // Redirigir al panel de administrador
+    } else {
+      res.status(200).json({ redirectURL: "./videos.html" }); // Redirigir a otra página para usuarios regulares
+    }
+  } catch (error) {
+    console.error("Error al autenticar usuario:", error);
+    res.status(500).json({ error: "Hubo un error al autenticar el usuario." });
+  }
+};
+
 module.exports = {
   usuarioRestringidoPost,
   usuarioRestringidoGet,
   usuarioRestringidoUpdate,
-  usuarioRestringidoDelete
+  usuarioRestringidoDelete,
+  loginPin
 };
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const dbUrl = require('./db/dbConfig');
+const mongoString = process.env.DATABASE_URL;
 const app = express();
 const PORT = 3000;
 
-mongoose.connect(dbUrl, {
+mongoose.connect(mongoString, {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true
@@ -19,22 +21,23 @@ const {
     usuarioRestringidoPost,
     usuarioRestringidoGet,
     usuarioRestringidoUpdate,
-    usuarioRestringidoDelete }
-     = require('./controllers/perfilesController.js');
+    usuarioRestringidoDelete,
+    loginPin
+} = require('./controllers/perfilesController.js');
 
 const { 
-        videoPost,
-        videoGet, 
-        videoDelete,
-        videoUpdate }
-         = require('./controllers/VIdeosController');    
+    videoPost,
+    videoGet, 
+    videoDelete,
+    videoUpdate
+} = require('./controllers/VIdeosController');    
 
 const { registroPost, login } = require("./controllers/registrosController.js");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-const cors = require("cors");
+
 app.use(cors({
     domains: '*',
     methods: "*"
@@ -55,8 +58,11 @@ app.post("/api/login", login);
 // Rutas de perfiles
 app.post('/api/perfiles',usuarioRestringidoPost);
 app.get('/api/perfiles', usuarioRestringidoGet);
-app.patch('/api/perfiles',usuarioRestringidoUpdate);
-app.delete('/api/perfiles',usuarioRestringidoDelete); 
+app.patch('/api/perfiles/:id',usuarioRestringidoUpdate); // Modificado para incluir el ID en la ruta
+app.delete('/api/perfiles',usuarioRestringidoDelete);
+
+// Ruta para verificar el PIN
+app.post('/api/loginPin', loginPin);
 
 app.listen(PORT, () => console.log(`Aplicación iniciando en el puerto ${PORT} !`));
 
